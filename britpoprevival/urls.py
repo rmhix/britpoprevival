8d3697bfb952080ca6a7f8e44e6e5ef39a741125
rmhix <rmhix@hotmail.com>
2026-03-13 12:12:27 +0000

    Updated settings, added .env loading, fixed login template

--- britpoprevival/urls.py
+++ britpoprevival/urls.py
@@ -32,5 +32,7 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="login.html", redirect_authenticated_user=True), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     path("register/", views.register, name="register"),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 
 ]
