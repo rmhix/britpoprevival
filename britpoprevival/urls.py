f9fafaab36ea44856959d7ea465c19e7b62ee9e1
rmhix <rmhix@hotmail.com>
2026-03-14 11:36:22 +0000

    fixed project urls to remove duplicate import

--- britpoprevival/urls.py
+++ britpoprevival/urls.py
@@ -16,15 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.urls import path, include
 from core import views
 
-from django.urls import path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
